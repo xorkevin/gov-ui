93326c93f7fc1d9f3ce212010bda586739f3b5c5
Kevin Wang <wangkevin448@gmail.com>
2017-09-07 02:54:07 -0700

    add cache control header

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const serveFile = (app, filename)=>{
 
 const serveIndex = async (req, res, next)=>{
   try {
+    res.set('Cache-Control', 'private, max-age=60');
     const {redirect, url, html, state} = await renderToString(req.url);
     if(redirect){
       res.redirect(302, url);
