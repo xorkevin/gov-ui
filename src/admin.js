a537de4f1918ded42822c5ed763b1939c0580367
Kevin Wang <wangkevin448@gmail.com>
2017-09-09 17:12:51 -0700

    toggle darkmode

--- src/admin.js
+++ src/admin.js
@@ -1,4 +1,4 @@
-import {h} from 'preact';
+import {h, Component} from 'preact';
 import {Switch, Route, Redirect, NavLink} from 'react-router-dom';
 
 import Loader from 'loader';
@@ -10,54 +10,82 @@ import Grid from 'component/grid';
 import Anchor from 'component/anchor';
 import FaIcon from 'component/faicon';
 
-const Admin = ({match})=>{
-  return <div>
-    <Navbar sidebar left={[
-      {key: 'home', component: <NavLink exact to="/"><FaIcon icon="home"/><small>Home</small></NavLink>},
-      {key: 'health', component: <NavLink to="/health"><FaIcon icon="server"/><small>Health</small></NavLink>},
-    ]} right={[
-      {key: 'xorkevin', component: <Anchor ext href="https://github.com/xorkevin"><FaIcon icon="github"/><small>xorkevin</small></Anchor>},
-    ]}>
-    </Navbar>
+class Admin extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dark: props.dark || false,
+    };
+  }
 
-    <MainContent withSidebar sectionNoMargin>
-      <Switch>
-        <Route exact path="/" component={Loader(()=>{return import('container/admin');}, (loaded, Admin)=>{
-          if(loaded){
-            return h(Admin);
-          }
-          return 'loading';
-        })}/>
-        <Route path="/health" component={Loader(()=>{return import('container/health');}, (loaded, Health)=>{
-          if(loaded){
-            return h(Health);
-          }
-          return 'loading';
-        })}/>
-        <Redirect to="/"/>
-      </Switch>
-    </MainContent>
+  setDark(){
+    this.setState((prevState)=>{
+      return Object.assign({}, prevState, {dark: true});
+    });
+  }
 
-    <Footer withSidebar>
-      <Grid center sm={8}>
-        <div colkey="left" className="text-center">
-         <h4>Nuke</h4>
-         a reactive frontend for governor
-        </div>
-        <div colkey="center" className="text-center">
-          <ul>
-            <li><Anchor noColor ext href="https://github.com/hackform/nuke"><FaIcon icon="github"/> Github</Anchor></li>
-            <li>Designed for <Anchor noColor ext href="https://github.com/hackform/governor">hackform/governor</Anchor></li>
-          </ul>
-        </div>
-        <div colkey="right" className="text-center">
-          <h5>
-            <FaIcon icon="code"/> with <FaIcon icon="heart-o"/> by <Anchor noColor ext href="https://github.com/xorkevin"><FaIcon icon="github"/> xorkevin</Anchor>
-          </h5>
-        </div>
-      </Grid>
-    </Footer>
-  </div>;
-};
+  setLight(){
+    this.setState((prevState)=>{
+      return Object.assign({}, prevState, {dark: false});
+    });
+  }
+
+  toggleDark(){
+    this.setState((prevState)=>{
+      return Object.assign({}, prevState, {dark: !prevState.dark});
+    });
+  }
+
+  render({}, {dark}){
+    return <div>
+      <Navbar sidebar left={[
+        {key: 'home', component: <NavLink exact to="/"><FaIcon icon="home"/><small>Home</small></NavLink>},
+        {key: 'health', component: <NavLink to="/health"><FaIcon icon="server"/><small>Health</small></NavLink>},
+      ]} right={[
+        {key: 'dark-mode', component: <span onClick={()=>{this.toggleDark();}}><FaIcon icon="bolt"/><small>Dark Mode</small></span>},
+        {key: 'xorkevin', component: <Anchor ext href="https://github.com/xorkevin"><FaIcon icon="github"/><small>xorkevin</small></Anchor>},
+      ]}>
+      </Navbar>
+
+      <MainContent withSidebar sectionNoMargin dark={dark}>
+        <Switch>
+          <Route exact path="/" component={Loader(()=>{return import('container/admin');}, (loaded, Admin)=>{
+            if(loaded){
+              return h(Admin);
+            }
+            return 'loading';
+          })}/>
+          <Route path="/health" component={Loader(()=>{return import('container/health');}, (loaded, Health)=>{
+            if(loaded){
+              return h(Health);
+            }
+            return 'loading';
+          })}/>
+          <Redirect to="/"/>
+        </Switch>
+      </MainContent>
+
+      <Footer withSidebar>
+        <Grid center sm={8}>
+          <div colkey="left" className="text-center">
+           <h4>Nuke</h4>
+           a reactive frontend for governor
+          </div>
+          <div colkey="center" className="text-center">
+            <ul>
+              <li><Anchor noColor ext href="https://github.com/hackform/nuke"><FaIcon icon="github"/> Github</Anchor></li>
+              <li>Designed for <Anchor noColor ext href="https://github.com/hackform/governor">hackform/governor</Anchor></li>
+            </ul>
+          </div>
+          <div colkey="right" className="text-center">
+            <h5>
+              <FaIcon icon="code"/> with <FaIcon icon="heart-o"/> by <Anchor noColor ext href="https://github.com/xorkevin"><FaIcon icon="github"/> xorkevin</Anchor>
+            </h5>
+          </div>
+        </Grid>
+      </Footer>
+    </div>;
+  }
+}
 
 export default Admin
--- src/component/maincontent.js
+++ src/component/maincontent.js
@@ -1,20 +1,49 @@
-import {h} from 'preact';
+import {h, Component} from 'preact';
+import {isWeb} from 'utility';
 
-const MainContent = ({withSidebar, withNavbar, sectionNoMargin, children})=>{
-  const k = [];
-  if(withSidebar){
-    k.push("with-sidebar");
-  }
-  if(withNavbar){
-    k.push("with-navbar");
-  }
-  if(sectionNoMargin){
-    k.push("section-no-margin");
+class MainContent extends Component {
+  constructor(props){
+    super(props);
+
+    if(isWeb()){
+      if(props.dark){
+        document.body.classList.add('dark');
+      }
+    }
   }
 
-  return <main className={k.join(" ")}>
-    {children}
-  </main>;
-};
+  componentWillReceiveProps(nextProps){
+    if(isWeb()){
+      if(nextProps.dark){
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+      }
+    }
+  }
+
+  componentWillUnmount(){
+    if(isWeb()){
+      document.body.classList.remove('dark');
+    }
+  }
+
+  render({withSidebar, withNavbar, sectionNoMargin, children}){
+    const k = [];
+    if(withSidebar){
+      k.push("with-sidebar");
+    }
+    if(withNavbar){
+      k.push("with-navbar");
+    }
+    if(sectionNoMargin){
+      k.push("section-no-margin");
+    }
+
+    return <main className={k.join(" ")}>
+      {children}
+    </main>;
+  }
+}
 
 export default MainContent
