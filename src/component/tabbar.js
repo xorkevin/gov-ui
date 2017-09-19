ce6755bc8eb05955327d11ac7632c7d8f2ab3f33
Kevin Wang <wangkevin448@gmail.com>
2017-09-18 20:57:04 -0700

    tab bar

---
+++ src/component/tabbar.js
@@ -0,0 +1,37 @@
+import {h, Component} from 'preact';
+import Container from 'component/container';
+
+const generateItemList = (list)=>{
+  return list.map((item)=>{
+    return <div key={item.key} className="item">{item.component}</div>;
+  });
+};
+
+class Tabbar extends Component {
+  render({left, right, children}){
+    const className = ["tabbar"];
+    let j = false;
+    let k = false;
+    if(left){
+      j = generateItemList(left);
+    }
+    if(right){
+      k = generateItemList(right);
+    }
+    return <div className={className.join(" ")}>
+      <Container>
+        <div className="element">
+          {j}
+        </div>
+        {children && children.length > 0 && <div className="element">
+          {children}
+        </div>}
+        <div className="element">
+          {k}
+        </div>
+      </Container>
+    </div>;
+  }
+}
+
+export default Tabbar
--- src/container/account/index.js
+++ src/container/account/index.js
@@ -1,12 +1,17 @@
 import {h, Component} from 'preact';
-import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
+import {Switch, Route, Redirect, NavLink, withRouter} from 'react-router-dom';
 import Section from 'component/section';
+import Tabbar from 'component/tabbar';
 
 import Loader from 'loader';
 
 class Account extends Component {
   render({match}){
     return <Section container narrow padded sectionTitle="Settings">
+      <Tabbar left={[
+        {key: 'account', component: <NavLink to={`${match.path}/account`}>Account</NavLink>},
+        {key: 'profile', component: <NavLink to={`${match.path}/profile`}>Profile</NavLink>},
+      ]}/>
       <Switch>
         <Route path={`${match.path}/account`} component={Loader(()=>{return import('container/account/details');})}/>
         <Route path={`${match.path}/profile/edit`} component={Loader(()=>{return import('container/account/profileedit');})}/>
