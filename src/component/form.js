58f2746ec81f3da6a7e14450ff7d31b625c4bcf3
Kevin Wang <wangkevin448@gmail.com>
2017-11-28 10:44:56 -0800

    update form enter keypress

--- src/component/form.js
+++ src/component/form.js
@@ -2,7 +2,7 @@ import {h, Component} from 'preact';
 import shortid from 'shortid';
 
 class Input extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       value: props.value,
@@ -12,79 +12,112 @@ class Input extends Component {
     this.id = shortid.generate();
   }
 
-  handleChange(event){
-    if(this.props.type && this.props.type === "file"){
-      if(event.target.files.length < 1){
-        this.setState((prevState)=>{
+  handleChange(event) {
+    if (this.props.type && this.props.type === 'file') {
+      if (event.target.files.length < 1) {
+        this.setState(prevState => {
           return Object.assign({}, prevState, {value: undefined});
         });
-        if(this.props.onChange){
+        if (this.props.onChange) {
           this.props.onChange(undefined);
         }
       } else {
-        this.setState((prevState)=>{
+        this.setState(prevState => {
           return Object.assign({}, prevState, {value: event.target.files[0]});
         });
-        if(this.props.onChange){
+        if (this.props.onChange) {
           this.props.onChange(event.target.files[0]);
         }
       }
     } else {
-      this.setState((prevState)=>{
+      this.setState(prevState => {
         return Object.assign({}, prevState, {value: event.target.value});
       });
-      if(this.props.onChange){
+      if (this.props.onChange) {
         this.props.onChange(event.target.value);
       }
     }
   }
 
-  handleEnter(target){
-    if(this.props.onEnter && target.charCode==13){
+  handleEnter(target) {
+    if (this.props.onEnter && target.key === 'Enter') {
       this.props.onEnter();
     }
   }
 
-  componentWillReceiveProps(nextProps){
-    if(nextProps.value){
-      this.setState((prevState)=>{
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.value) {
+      this.setState(prevState => {
         return Object.assign({}, prevState, {value: nextProps.value});
       });
     }
   }
 
-  render({valid, error, fullWidth, textarea, label, type, accept, capture, info}, {value}){
-    let k = ["input"];
-    if(valid){
-      k.push("valid");
-    } else if(error){
-      k.push("invalid");
+  render(
+    {valid, error, fullWidth, textarea, label, type, accept, capture, info},
+    {value},
+  ) {
+    let k = ['input'];
+    if (valid) {
+      k.push('valid');
+    } else if (error) {
+      k.push('invalid');
     }
 
-    if(type === 'checkbox'){
-      k.push("checkbox");
+    if (type === 'checkbox') {
+      k.push('checkbox');
     }
 
-    if(fullWidth){
-      k.push("full-width");
+    if (fullWidth) {
+      k.push('full-width');
     }
 
     let inp = false;
-    if(textarea){
-      inp = <textarea id={this.id} value={value} onInput={this.handleChange} onKeyPress={this.handleEnter} placeholder=" "></textarea>;
-    } else if(type && (type === 'file' || type === 'checkbox')){
-      inp = <input id={this.id} type={type} accept={accept} capture={capture} value={value} onChange={this.handleChange} onKeyPress={this.handleEnter} placeholder=" "/>;
+    if (textarea) {
+      inp = (
+        <textarea
+          id={this.id}
+          value={value}
+          onInput={this.handleChange}
+          onKeyPress={this.handleEnter}
+          placeholder=" "
+        />
+      );
+    } else if (type && (type === 'file' || type === 'checkbox')) {
+      inp = (
+        <input
+          id={this.id}
+          type={type}
+          accept={accept}
+          capture={capture}
+          value={value}
+          onChange={this.handleChange}
+          onKeyPress={this.handleEnter}
+          placeholder=" "
+        />
+      );
     } else {
-      inp = <input id={this.id} type={type} value={value} onInput={this.handleChange} onKeyPress={this.handleEnter} placeholder=" "/>;
+      inp = (
+        <input
+          id={this.id}
+          type={type}
+          value={value}
+          onInput={this.handleChange}
+          onKeyPress={this.handleEnter}
+          placeholder=" "
+        />
+      );
     }
 
-    return <div className={k.join(" ")}>
-      {inp}
-      <label htmlFor={this.id}>{label}</label>
-      {!error && info && <span className="info">{info}</span>}
-      {error && <span className="error">{error}</span>}
-    </div>;
+    return (
+      <div className={k.join(' ')}>
+        {inp}
+        <label htmlFor={this.id}>{label}</label>
+        {!error && info && <span className="info">{info}</span>}
+        {error && <span className="error">{error}</span>}
+      </div>
+    );
   }
 }
 
-export default Input
+export default Input;
