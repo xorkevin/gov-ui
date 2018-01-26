d490ef26c9f0645c0474b2b6c2e8015051f9deed
Kevin Wang <wangkevin448@gmail.com>
2018-01-25 22:54:28 -0800

    Bugfix get dark from settings reducer

--- src/app.js
+++ src/app.js
@@ -15,69 +15,139 @@ import Anchor from 'component/anchor';
 import FaIcon from 'component/faicon';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.toggleDark = this.toggleDark.bind(this);
   }
 
-  toggleDark(){
+  toggleDark() {
     this.props.toggleDark();
   }
 
-  render({}, {dark}){
-    return <div>
-      <Navbar hideOnScroll left={[
-        {key: 'home', home: true, component: <NavLink exact to="/">Home</NavLink>},
-        {key: 'form', component: <NavLink to="/form">Form</NavLink>},
-        {key: 'cards', component: <NavLink to="/cards">Cards</NavLink>},
-        {key: 'health', component: <NavLink to="/health">Health</NavLink>},
-      ]} right={[
-        {key: 'settings', component: <Menu icon={<span><FaIcon icon="cog"/> Settings</span>} size="md" fixed align="right" position="bottom">
-          <span onClick={this.toggleDark}><FaIcon icon="bolt"/> Dark Mode</span>
-          <Anchor ext href="https://github.com/xorkevin"><FaIcon icon="github"/> xorkevin</Anchor>
-        </Menu>},
-      ]}>
-      </Navbar>
+  render({dark}, {}) {
+    return (
+      <div>
+        <Navbar
+          hideOnScroll
+          left={[
+            {
+              key: 'home',
+              home: true,
+              component: (
+                <NavLink exact to="/">
+                  Home
+                </NavLink>
+              ),
+            },
+            {key: 'form', component: <NavLink to="/form">Form</NavLink>},
+            {key: 'cards', component: <NavLink to="/cards">Cards</NavLink>},
+            {key: 'health', component: <NavLink to="/health">Health</NavLink>},
+          ]}
+          right={[
+            {
+              key: 'settings',
+              component: (
+                <Menu
+                  icon={
+                    <span>
+                      <FaIcon icon="cog" /> Settings
+                    </span>
+                  }
+                  size="md"
+                  fixed
+                  align="right"
+                  position="bottom">
+                  <span onClick={this.toggleDark}>
+                    <FaIcon icon="bolt" /> Dark Mode
+                  </span>
+                  <Anchor ext href="https://github.com/xorkevin">
+                    <FaIcon icon="github" /> xorkevin
+                  </Anchor>
+                </Menu>
+              ),
+            },
+          ]}
+        />
 
-      <MainContent dark={dark}>
-        <Switch>
-          <Route exact path="/" component={Loader(()=>{return import('container/home');})}/>
-          <Route path="/form" component={Loader(()=>{return import('container/form');})}/>
-          <Route path="/cards" component={Loader(()=>{return import('container/card');})}/>
-          <Route path="/health" component={Loader(()=>{return import('container/health');})}/>
-          <Redirect to="/"/>
-        </Switch>
-      </MainContent>
+        <MainContent dark={dark}>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={Loader(() => {
+                return import('container/home');
+              })}
+            />
+            <Route
+              path="/form"
+              component={Loader(() => {
+                return import('container/form');
+              })}
+            />
+            <Route
+              path="/cards"
+              component={Loader(() => {
+                return import('container/card');
+              })}
+            />
+            <Route
+              path="/health"
+              component={Loader(() => {
+                return import('container/health');
+              })}
+            />
+            <Redirect to="/" />
+          </Switch>
+        </MainContent>
 
-      <Footer>
-        <Grid center map sm={8}>
-          <div colkey="left" className="text-center">
-           <h4>Nuke</h4>
-           a reactive frontend for governor
-          </div>
-          <div colkey="center" className="text-center">
-            <ul>
-              <li><Anchor noColor ext href="https://github.com/hackform/nuke"><FaIcon icon="github"/> Github</Anchor></li>
-              <li>Designed for <Anchor noColor ext href="https://github.com/hackform/governor">hackform/governor</Anchor></li>
-            </ul>
-          </div>
-          <div colkey="right" className="text-center">
-            <h5>
-              <FaIcon icon="code"/> with <FaIcon icon="heart-o"/> by <Anchor noColor ext href="https://github.com/xorkevin"><FaIcon icon="github"/> xorkevin</Anchor>
-            </h5>
-          </div>
-        </Grid>
-      </Footer>
-    </div>;
+        <Footer>
+          <Grid center map sm={8}>
+            <div colkey="left" className="text-center">
+              <h4>Nuke</h4>
+              a reactive frontend for governor
+            </div>
+            <div colkey="center" className="text-center">
+              <ul>
+                <li>
+                  <Anchor noColor ext href="https://github.com/hackform/nuke">
+                    <FaIcon icon="github" /> Github
+                  </Anchor>
+                </li>
+                <li>
+                  Designed for{' '}
+                  <Anchor
+                    noColor
+                    ext
+                    href="https://github.com/hackform/governor">
+                    hackform/governor
+                  </Anchor>
+                </li>
+              </ul>
+            </div>
+            <div colkey="right" className="text-center">
+              <h5>
+                <FaIcon icon="code" /> with <FaIcon icon="heart-o" /> by{' '}
+                <Anchor noColor ext href="https://github.com/xorkevin">
+                  <FaIcon icon="github" /> xorkevin
+                </Anchor>
+              </h5>
+            </div>
+          </Grid>
+        </Footer>
+      </div>
+    );
   }
 }
-const mapStateToProps = (state)=>{
-  return {};
+const mapStateToProps = state => {
+  const {dark} = state.Settings;
+  return {
+    dark,
+  };
 };
 
-const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = dispatch => {
   return {
-    toggleDark: ()=>{
+    toggleDark: () => {
       dispatch(DarkMode());
     },
   };
@@ -86,4 +156,4 @@ const mapDispatchToProps = (dispatch)=>{
 App = connect(mapStateToProps, mapDispatchToProps)(App);
 App = withRouter(App); // needed to force rerender on location change
 
-export default App
+export default App;
