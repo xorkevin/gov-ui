9f1719ac4fbb95f19c6a2c49bda29e46b22448c3
Kevin Wang <wangkevin448@gmail.com>
2018-03-14 17:12:43 -0700

    Remove battery and swpreload

--- src/battery/index.js
+++
@@ -1,7 +0,0 @@
-import Battery from './battery';
-import Terminal from './terminal';
-import Connector from './connector';
-
-export {
-  Battery, Terminal, Connector,
-}
--- src/container/health.js
+++ src/container/health.js
@@ -2,43 +2,42 @@ import {h, Component} from 'preact';
 import Section from 'component/section';
 
 import {connect} from 'preact-redux';
-import {Connector} from 'battery';
 import {TimeGet} from 'reducer/health';
 
 class HealthContainer extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.getTime();
   }
 
-  render({loading, success, time, err}){
-    return <Section sectionTitle="Health Check" container padded>
-      <span>{loading && "loading"}</span>
-      <span>{success && time}</span>
-      <span>{!success && err}</span>
-    </Section>;
+  render({loading, success, time, err}) {
+    return (
+      <Section sectionTitle="Health Check" container padded>
+        <span>{loading && 'loading'}</span>
+        <span>{success && time}</span>
+        <span>{!success && err}</span>
+      </Section>
+    );
   }
 }
 
-const mapStateToProps = (state)=>{
+const mapStateToProps = state => {
   const {time, err, loading, success} = state.Health;
   return {
-    loading, success, time, err,
+    loading,
+    success,
+    time,
+    err,
   };
 };
 
-const mapDispatchToProps = (dispatch)=>{
+const mapDispatchToProps = dispatch => {
   return {
-    getTime: ()=>{
+    getTime: () => {
       dispatch(TimeGet());
     },
   };
 };
 
-const contracts = [
-  TimeGet(),
-];
-
 HealthContainer = connect(mapStateToProps, mapDispatchToProps)(HealthContainer);
-HealthContainer = Connector(contracts)(HealthContainer);
 
-export default HealthContainer
+export default HealthContainer;
