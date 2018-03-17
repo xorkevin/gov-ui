59f6c56400a3bba6bc4fe33ae5a0b2fe65436444
Kevin Wang <wangkevin448@gmail.com>
2018-03-17 14:41:15 -0700

    Bugfix tooltip by adding parent display block option

--- src/component/tooltip.js
+++ src/component/tooltip.js
@@ -1,19 +1,31 @@
 import {h, Component} from 'preact';
 
-const Tooltip = ({position, tooltip, children})=>{
-  const k = ["tooltip"];
-  switch(position){
-    case "left":
-    case "right":
-    case "top":
-    case "bottom":
+const Tooltip = ({position, tooltip, children}) => {
+  const k = ['tooltip'];
+  switch (position) {
+    case 'left':
+    case 'right':
+    case 'top':
+    case 'bottom':
       k.push(position);
       break;
     default:
-      k.push("top");
+      k.push('top');
   }
 
-  return <span className="tooltip-parent"><span className={k.join(" ")}>{tooltip}</span>{children && children.length > 0 && children[0]}</span>;
+  const j = ['tooltip-parent'];
+  switch (display) {
+    case 'inline':
+    case 'block':
+      j.push(display);
+  }
+
+  return (
+    <span className={j.join(' ')}>
+      <span className={k.join(' ')}>{tooltip}</span>
+      {children && children.length > 0 && children[0]}
+    </span>
+  );
 };
 
-export default Tooltip
+export default Tooltip;
