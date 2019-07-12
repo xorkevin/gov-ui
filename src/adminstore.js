4bfee51c07e3a54ecef122ccc5549b09f418bd60
Kevin Wang <kevin@xorkevin.com>
2019-07-11 22:54:16 -0700

    Remove remaining unused reducers

--- src/adminstore.js
+++ src/adminstore.js
@@ -1,13 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
-import Health from 'reducer/health';
 import Settings from 'service/settings';
 import Auth from 'service/auth';
 
 const store = createStore(
   combineReducers({
-    Health,
     Settings,
     Auth,
   }),
--- src/store.js
+++ src/store.js
@@ -1,12 +1,10 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
-import Health from 'reducer/health';
 import Settings from 'service/settings';
 
 const store = createStore(
   combineReducers({
-    Health,
     Settings,
   }),
   applyMiddleware(thunk),
