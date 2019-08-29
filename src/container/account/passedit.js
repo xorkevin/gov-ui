b45ccc834acad703dd8d545ca1deadb8528cf9a5
Kevin Wang <kevin@xorkevin.com>
2019-08-28 18:03:59 -0700

    Refactor passedit to use form context

--- src/container/account/passedit.js
+++ src/container/account/passedit.js
@@ -1,32 +1,61 @@
-import React, {Fragment, useCallback} from 'react';
+import React, {Fragment} from 'react';
 import {Link} from 'react-router-dom';
 import {useAuthCall} from 'service/auth';
+import {useSnackbarView} from 'service/snackbar';
 import Section from 'component/section';
 import Card from 'component/card';
 import Button from 'component/button';
-import Input, {useForm} from 'component/form';
+import {Form, Input, useForm} from 'component/form';
 
 const selectAPIEditPass = (api) => api.u.user.pass.edit;
 
+const formErrCheck = ({new_password, password_confirm}) => {
+  const err = {};
+  if (new_password.length > 0 && new_password.length < 10) {
+    err.new_password = true;
+  }
+  if (password_confirm.length > 0 && password_confirm !== new_password) {
+    err.password_confirm = 'Must match password';
+  }
+  return err;
+};
+
+const formValidCheck = ({new_password, password_confirm}) => {
+  const valid = {};
+  if (new_password.length > 9) {
+    valid.new_password = true;
+  }
+  if (password_confirm.length > 0 && password_confirm === new_password) {
+    valid.password_confirm = true;
+  }
+  return valid;
+};
+
+const prehookValidate = ([form]) => {
+  const {new_password, password_confirm} = form;
+  if (new_password !== password_confirm) {
+    return 'Passwords do not match';
+  }
+};
+
 const AccountPassEdit = () => {
+  const displaySnackbar = useSnackbarView(
+    <Fragment>
+      <span>Password updated</span>
+    </Fragment>,
+  );
+
   const [formState, updateForm] = useForm({
     old_password: '',
     new_password: '',
     password_confirm: '',
   });
 
-  const prehook = useCallback(([form]) => {
-    const {new_password, password_confirm} = form;
-    if (new_password !== password_confirm) {
-      return 'Passwords do not match';
-    }
-  }, []);
-
   const [passState, execEditPass] = useAuthCall(
     selectAPIEditPass,
     [formState.old_password, formState.new_password],
     {},
-    {prehook},
+    {prehook: prehookValidate, posthook: displaySnackbar},
   );
 
   const {success, err} = passState;
@@ -51,34 +80,35 @@ const AccountPassEdit = () => {
   return (
     <Card size="md" restrictWidth center bar={bar}>
       <Section subsection sectionTitle="Account Details">
-        <Input
-          label="old password"
-          type="password"
-          name="old_password"
-          value={formState.old_password}
-          onChange={updateForm}
-          fullWidth
-        />
-        <Input
-          label="new password"
-          type="password"
-          name="new_password"
-          value={formState.new_password}
-          onChange={updateForm}
-          fullWidth
-        />
-        <Input
-          label="confirm password"
-          type="password"
-          name="password_confirm"
-          value={formState.password_confirm}
+        <Form
+          formState={formState}
           onChange={updateForm}
           onEnter={execEditPass}
-          fullWidth
-        />
+          errCheck={formErrCheck}
+          validCheck={formValidCheck}
+        >
+          <Input
+            label="old password"
+            type="password"
+            name="old_password"
+            fullWidth
+          />
+          <Input
+            label="new password"
+            type="password"
+            name="new_password"
+            info="Must be at least 10 characters"
+            fullWidth
+          />
+          <Input
+            label="confirm password"
+            type="password"
+            name="password_confirm"
+            fullWidth
+          />
+        </Form>
       </Section>
       {err && <span>{err}</span>}
-      {success && <span>Password updated</span>}
     </Card>
   );
 };
