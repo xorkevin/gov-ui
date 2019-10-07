92e73dd018401959bb84fecd6f57a19989bcd19d
Kevin Wang <kevin@xorkevin.com>
2019-10-07 16:39:12 -0700

    Add list group styles

--- src/component/listgroup.stories.js
+++ src/component/listgroup.stories.js
@@ -5,28 +5,56 @@ export default {title: 'List Group'};
 
 export const plain = () => (
   <ListGroup>
-    <ListItem>here is a list</ListItem>
-    <ListItem>of some items</ListItem>
+    <ListItem>
+      <div>here is a list</div>
+    </ListItem>
+    <ListItem>
+      <div>of some items</div>
+    </ListItem>
+    <ListItem>
+      <div>in a list group</div>
+    </ListItem>
   </ListGroup>
 );
 
 export const sm = () => (
   <ListGroup size="sm">
-    <ListItem>here is a list</ListItem>
-    <ListItem>of some items</ListItem>
+    <ListItem>
+      <div>here is a list</div>
+    </ListItem>
+    <ListItem>
+      <div>of some items</div>
+    </ListItem>
+    <ListItem>
+      <div>in a list group</div>
+    </ListItem>
   </ListGroup>
 );
 
 export const md = () => (
   <ListGroup size="md">
-    <ListItem>here is a list</ListItem>
-    <ListItem>of some items</ListItem>
+    <ListItem>
+      <div>here is a list</div>
+    </ListItem>
+    <ListItem>
+      <div>of some items</div>
+    </ListItem>
+    <ListItem>
+      <div>in a list group</div>
+    </ListItem>
   </ListGroup>
 );
 
 export const lg = () => (
   <ListGroup size="lg">
-    <ListItem>here is a list</ListItem>
-    <ListItem>of some items</ListItem>
+    <ListItem>
+      <div>here is a list</div>
+    </ListItem>
+    <ListItem>
+      <div>of some items</div>
+    </ListItem>
+    <ListItem>
+      <div>in a list group</div>
+    </ListItem>
   </ListGroup>
 );
