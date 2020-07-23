04e21e5ed6a0d41d67ccc73dfddd7461d8e3d227
Kevin Wang <kevin@xorkevin.com>
2020-07-22 23:26:00 -0700

    Begin refactor app home

--- example/src/app.js
+++ example/src/app.js
@@ -1,146 +1,163 @@
 import React, {Fragment, lazy, Suspense} from 'react';
-import {Switch, Route, Redirect, NavLink, Link} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {useAuthValue, useLogout} from '@xorkevin/turbine';
 import {
-  useAuthValue,
-  useLogout,
-  Protected,
-  AntiProtected,
-} from '@xorkevin/turbine';
-import {
-  useDarkMode,
+  useDarkModeValue,
+  useSetDarkMode,
   SnackbarContainer,
   MainContent,
-  Section,
+  Container,
   Navbar,
-  Navitem,
+  NavItem,
+  useMenu,
   Menu,
+  MenuItem,
+  MenuHeader,
+  MenuDivider,
   Footer,
   Grid,
-  Anchor,
+  Column,
   FaIcon,
 } from '@xorkevin/nuke';
+import AnchorSecondary from '@xorkevin/nuke/src/component/anchor/secondary';
 
-const AdminContainer = Protected(lazy(() => import('admin')));
-const LoginContainer = AntiProtected(
-  lazy(() => import('@xorkevin/gov-ui/src/container/login')),
-);
-const AccountContainer = Protected(
-  lazy(() => import('@xorkevin/gov-ui/src/container/account')),
-);
-const DeveloperContainer = Protected(
-  lazy(() => import('@xorkevin/gov-ui/src/container/developer')),
-);
-const UserContainer = Protected(
-  lazy(() => import('@xorkevin/gov-ui/src/container/user')),
-);
-const ManageContainer = lazy(() =>
-  import('@xorkevin/gov-ui/src/container/manage'),
-);
-const HealthContainer = Protected(
-  lazy(() => import('health')),
-  'admin',
-);
-const CourierContainer = Protected(
-  lazy(() => import('@xorkevin/gov-ui/src/container/courier')),
-  ['admin', 'usr_courier'],
-);
-const SetupContainer = lazy(() =>
-  import('@xorkevin/gov-ui/src/container/setup'),
-);
+//const AdminContainer = Protected(lazy(() => import('admin')));
+const AdminContainer = lazy(() => import('admin'));
+//const LoginContainer = AntiProtected(
+//  lazy(() => import('@xorkevin/gov-ui/src/container/login')),
+//);
+//const AccountContainer = Protected(
+//  lazy(() => import('@xorkevin/gov-ui/src/container/account')),
+//);
+//const DeveloperContainer = Protected(
+//  lazy(() => import('@xorkevin/gov-ui/src/container/developer')),
+//);
+//const UserContainer = Protected(
+//  lazy(() => import('@xorkevin/gov-ui/src/container/user')),
+//);
+//const ManageContainer = lazy(() =>
+//  import('@xorkevin/gov-ui/src/container/manage'),
+//);
+//const HealthContainer = Protected(
+//  lazy(() => import('health')),
+//  'admin',
+//);
+//const CourierContainer = Protected(
+//  lazy(() => import('@xorkevin/gov-ui/src/container/courier')),
+//  ['admin', 'usr_courier'],
+//);
+//const SetupContainer = lazy(() =>
+//  import('@xorkevin/gov-ui/src/container/setup'),
+//);
 
 const FallbackView = (
-  <Section container padded narrow>
+  <Container padded narrow>
     Loading
-  </Section>
+  </Container>
 );
 
-const GovContextValue = Object.freeze({
-  homePath: '/',
-  // eslint-disable-next-line no-undef
-  courierPath: COURIERBASE_URL,
-});
+//const GovContextValue = Object.freeze({
+//  homePath: '/',
+//  // eslint-disable-next-line no-undef
+//  courierPath: COURIERBASE_URL,
+//});
 
 const App = () => {
-  const [dark, toggleDark] = useDarkMode();
+  const dark = useDarkModeValue();
+  const toggleDark = useSetDarkMode();
+  const menu = useMenu();
   const logout = useLogout();
   const {loggedIn} = useAuthValue();
 
   return (
     <div>
-      {loggedIn && (
-        <Navbar
-          sidebar
-          left={
-            <Fragment>
-              <Navitem>
-                <NavLink exact to="/">
-                  <FaIcon icon="home" />
-                  <small>Home</small>
-                </NavLink>
-              </Navitem>
-              <Navitem>
-                <NavLink to="/health">
-                  <FaIcon icon="server" />
-                  <small>Health</small>
-                </NavLink>
-              </Navitem>
-              <Navitem>
-                <NavLink to="/manage">
-                  <FaIcon icon="building" />
-                  <small>Manage</small>
-                </NavLink>
-              </Navitem>
-              <Navitem>
-                <NavLink to="/courier">
-                  <FaIcon icon="paper-plane" />
-                  <small>Courier</small>
-                </NavLink>
-              </Navitem>
-            </Fragment>
-          }
-          right={
-            <Fragment>
-              <Navitem>
-                <Menu
-                  icon={
-                    <Fragment>
-                      <FaIcon icon="cog" /> <small>Settings</small>
-                    </Fragment>
-                  }
-                  size="md"
-                  fixed
-                  align="left"
-                  position="top"
+      <Navbar
+        right={
+          <Fragment>
+            <NavItem forwardedRef={menu.anchorRef} onClick={menu.toggle}>
+              <FaIcon icon="cog" />
+            </NavItem>
+            {menu.show && (
+              <Menu size="md" anchor={menu.anchor} close={menu.close}>
+                <MenuHeader>Settings</MenuHeader>
+                {loggedIn && (
+                  <Fragment>
+                    <MenuItem
+                      local
+                      link="/a"
+                      icon={<FaIcon icon="address-card-o" />}
+                    >
+                      Account
+                    </MenuItem>
+                    <MenuItem local link="/dev" icon={<FaIcon icon="code" />}>
+                      Developer
+                    </MenuItem>
+                  </Fragment>
+                )}
+                <MenuItem
+                  onClick={toggleDark}
+                  icon={<FaIcon icon="bolt" />}
+                  label="Ctrl+B"
                 >
-                  <Link to="/a">
-                    <FaIcon icon="address-card-o" /> Account
-                  </Link>
-                  <Link to="/dev">
-                    <FaIcon icon="code" /> Developer
-                  </Link>
-                  <span onClick={toggleDark}>
-                    <FaIcon icon="bolt" /> {dark ? 'Light' : 'Dark'} Mode
-                  </span>
-                  <Anchor ext href="https://github.com/xorkevin">
-                    <FaIcon icon="github" /> xorkevin
-                  </Anchor>
-                  <span onClick={logout}>
-                    <FaIcon icon="sign-out" /> Sign out
-                  </span>
-                </Menu>
-              </Navitem>
-            </Fragment>
-          }
-        />
-      )}
+                  {dark ? 'Light' : 'Dark'} Mode
+                </MenuItem>
+                {loggedIn && (
+                  <MenuItem onClick={logout} icon={<FaIcon icon="sign-out" />}>
+                    Sign out
+                  </MenuItem>
+                )}
+                <MenuDivider />
+                <MenuHeader>About</MenuHeader>
+                <MenuItem
+                  link="https://github.com/xorkevin/gov-ui"
+                  ext
+                  icon={<FaIcon icon="github" />}
+                  label={<FaIcon icon="external-link" />}
+                >
+                  gov-ui
+                </MenuItem>
+                <MenuItem
+                  link="https://xorkevin.com/"
+                  ext
+                  icon={<FaIcon icon="globe-w" />}
+                  label={<FaIcon icon="external-link" />}
+                >
+                  xorkevin
+                </MenuItem>
+              </Menu>
+            )}
+          </Fragment>
+        }
+      >
+        <NavItem local link="/">
+          <FaIcon icon="home" />
+          <small>Home</small>
+        </NavItem>
+        {loggedIn && (
+          <Fragment>
+            <NavItem local link="/health">
+              <FaIcon icon="server" />
+              <small>Health</small>
+            </NavItem>
+            <NavItem local link="/manage">
+              <FaIcon icon="building" />
+              <small>Manage</small>
+            </NavItem>
+            <NavItem local link="/courier">
+              <FaIcon icon="paper-plane" />
+              <small>Courier</small>
+            </NavItem>
+          </Fragment>
+        )}
+      </Navbar>
 
-      <MainContent withSidebar={loggedIn} sectionNoMargin>
+      <MainContent>
         <Suspense fallback={FallbackView}>
           <Switch>
             <Route exact path="/">
               <AdminContainer />
             </Route>
-            <Route path="/x">
+            {/*<Route path="/x">
               <LoginContainer />
             </Route>
             <Route path="/a">
@@ -164,39 +181,44 @@ const App = () => {
             <Route path="/setup">
               <SetupContainer homePath={GovContextValue.homePath} />
             </Route>
+            */}
             <Redirect to="/" />
           </Switch>
         </Suspense>
       </MainContent>
 
-      <Footer withSidebar={loggedIn}>
-        <Grid map center sm={8}>
-          <div className="text-center">
-            <h4>Nuke</h4>a reactive frontend for governor
-          </div>
-          <div className="text-center">
-            <ul>
-              <li>
-                <Anchor noColor ext href="https://github.com/xorkevin/nuke">
-                  <FaIcon icon="github" /> Github
-                </Anchor>
-              </li>
-              <li>
-                Designed for{' '}
-                <Anchor noColor ext href="https://github.com/xorkevin/governor">
-                  xorkevin/governor
-                </Anchor>
-              </li>
-            </ul>
-          </div>
-          <div className="text-center">
-            <h5>
-              <FaIcon icon="code" /> with <FaIcon icon="heart-o" /> by{' '}
-              <Anchor noColor ext href="https://github.com/xorkevin">
-                <FaIcon icon="github" /> xorkevin
-              </Anchor>
-            </h5>
-          </div>
+      <Footer>
+        <Grid className="dark" justify="center" align="center">
+          <Column sm={8}>
+            <div className="text-center">
+              <h4>Gov UI</h4> a reactive frontend for governor
+              <ul>
+                <li>
+                  <AnchorSecondary
+                    ext
+                    href="https://github.com/xorkevin/gov-ui"
+                  >
+                    <FaIcon icon="github" /> Github
+                  </AnchorSecondary>
+                </li>
+                <li>
+                  Designed for{' '}
+                  <AnchorSecondary
+                    ext
+                    href="https://github.com/xorkevin/governor"
+                  >
+                    xorkevin/governor
+                  </AnchorSecondary>
+                </li>
+              </ul>
+              <h5>
+                <FaIcon icon="code" /> with <FaIcon icon="heart-o" /> by{' '}
+                <AnchorSecondary ext href="https://xorkevin.com/">
+                  <FaIcon icon="github" /> xorkevin
+                </AnchorSecondary>
+              </h5>
+            </div>
+          </Column>
         </Grid>
       </Footer>
       <SnackbarContainer />
