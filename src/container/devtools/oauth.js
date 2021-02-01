7a5313756f7ec7c26b02c54f479a73468a89d9a3
Kevin Wang <kevin@xorkevin.com>
2021-01-31 17:52:24 -0800

    Add openid config display

--- src/container/devtools/oauth.js
+++ src/container/devtools/oauth.js
@@ -1,20 +1,27 @@
 import {Fragment, useCallback, useMemo, useContext} from 'react';
 import {Switch, Route, Redirect, useRouteMatch} from 'react-router-dom';
 import {useResource} from '@xorkevin/substation';
+import {useAuthResource} from '@xorkevin/turbine';
 import {
   Grid,
   Column,
   Field,
   FieldSelect,
+  FieldSearchSelect,
   FieldMultiSelect,
   Form,
   useForm,
+  Description,
+  Chip,
 } from '@xorkevin/nuke';
 
 import {GovUICtx} from '../../middleware';
 import {randomID} from '../../utility';
 
 const selectAPIOidConfig = (api) => api.wellknown.openidconfig;
+const selectAPIApps = (api) => api.oauth.app.get;
+
+const OAUTHAPP_LIMIT = 32;
 
 const responseTypeOpts = [{display: 'code', value: 'code'}];
 const responseModeOpts = [
@@ -34,6 +41,19 @@ const challengeMethodOpts = [
   {display: 'plain', value: 'plain'},
   {display: 'SHA-256', value: 'S256'},
 ];
+
+const ChipList = ({list}) => {
+  if (!Array.isArray(list)) {
+    return null;
+  }
+  return (
+    <Fragment>
+      {list.map((i) => (
+        <Chip key={i}>{i}</Chip>
+      ))}
+    </Fragment>
+  );
+};
 
 const OAuthTool = ({pathCallback}) => {
   const ctx = useContext(GovUICtx);
@@ -78,6 +98,16 @@ const OAuthTool = ({pathCallback}) => {
     {posthook: posthookConfig},
   );
 
+  const [apps] = useAuthResource(selectAPIApps, [OAUTHAPP_LIMIT, 0], []);
+
+  const clientOpts = useMemo(
+    () =>
+      apps.success
+        ? apps.data.map((i) => ({display: i.name, value: i.client_id}))
+        : [],
+    [apps],
+  );
+
   const onSubmit = useCallback(() => {
     console.log('sent auth request');
   }, []);
@@ -86,7 +116,7 @@ const OAuthTool = ({pathCallback}) => {
     <div>
       <h3>OAuth Tester</h3>
       <Grid>
-        <Column md={16}>
+        <Column md={12}>
           <Form
             formState={form.state}
             onChange={form.update}
@@ -94,6 +124,13 @@ const OAuthTool = ({pathCallback}) => {
           >
             <Grid>
               <Column md={12}>
+                <FieldSearchSelect
+                  name="clientid"
+                  label="Client"
+                  options={clientOpts}
+                  nohint
+                  fullWidth
+                />
                 <Field name="clientid" label="Client ID" nohint fullWidth />
                 <Field
                   name="redirecturi"
@@ -158,10 +195,81 @@ const OAuthTool = ({pathCallback}) => {
             </Grid>
           </Form>
         </Column>
-        <Column md={8}>
+        <Column md={12}>
+          <h4>Openid Configuration</h4>
           {oidConfig.success && (
-            <Fragment>{JSON.stringify(oidConfig.data, null, 2)}</Fragment>
+            <Fragment>
+              <Description label="Issuer" item={oidConfig.data.issuer} />
+              <Description
+                label="Authorization Endpoint"
+                item={oidConfig.data.authorization_endpoint}
+              />
+              <Description
+                label="Token Endpoint"
+                item={oidConfig.data.token_endpoint}
+              />
+              <Description
+                label="Userinfo Endpoint"
+                item={oidConfig.data.userinfo_endpoint}
+              />
+              <Description label="JWKs URI" item={oidConfig.data.jwks_uri} />
+              <Description
+                label="Scopes Supported"
+                item={<ChipList list={oidConfig.data.scopes_supported} />}
+              />
+              <Description
+                label="Response Types Supported"
+                item={
+                  <ChipList list={oidConfig.data.response_types_supported} />
+                }
+              />
+              <Description
+                label="Response Modes Supported"
+                item={
+                  <ChipList list={oidConfig.data.response_modes_supported} />
+                }
+              />
+              <Description
+                label="Grant Types Supported"
+                item={<ChipList list={oidConfig.data.grant_types_supported} />}
+              />
+              <Description
+                label="Subject Types Supported"
+                item={
+                  <ChipList list={oidConfig.data.subject_types_supported} />
+                }
+              />
+              <Description
+                label="ID Token Signing Algorithms Supported"
+                item={
+                  <ChipList
+                    list={oidConfig.data.id_token_signing_alg_values_supported}
+                  />
+                }
+              />
+              <Description
+                label="Token Endpoint Auth Methods Supported"
+                item={
+                  <ChipList
+                    list={oidConfig.data.token_endpoint_auth_methods_supported}
+                  />
+                }
+              />
+              <Description
+                label="Code Challenge Methods Supported"
+                item={
+                  <ChipList
+                    list={oidConfig.data.code_challenge_methods_supported}
+                  />
+                }
+              />
+              <Description
+                label="Claims Supported"
+                item={<ChipList list={oidConfig.data.claims_supported} />}
+              />
+            </Fragment>
           )}
+          {oidConfig.err && <p>{oidConfig.err.message}</p>}
         </Column>
       </Grid>
     </div>
